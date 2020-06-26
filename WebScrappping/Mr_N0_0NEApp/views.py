60f470caf6b205c40efafb96f9899474c2632a99
Likith Gowda K <likithkrish31@gmail.com>
2020-06-26 21:51:07 +0530

    Adding_serachBar_to_html

--- WebScrappping/Mr_N0_0NEApp/views.py
+++ WebScrappping/Mr_N0_0NEApp/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search': search
+    }
+    return render(request,'Mr_NO_ONEApp/new_search.html', stuff_for_frontend)
